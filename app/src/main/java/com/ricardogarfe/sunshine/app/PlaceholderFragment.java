34a4cefa8f708c35b81a68752db8b823cd093c64
Ricardo García Fernández <ricardogarfe@gmail.com>
2015-11-08 11:48:54 +0100

    [ci  skip] - Fake data

--- app/src/main/java/com/ricardogarfe/sunshine/app/PlaceholderFragment.java
+++ app/src/main/java/com/ricardogarfe/sunshine/app/PlaceholderFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +20,13 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        List<String> weatherPrevisions = new ArrayList<>();
+        weatherPrevisions.add("Today - Sunny - 88/63");
+        weatherPrevisions.add("Tomorrow - Sunny - 88/63");
+        weatherPrevisions.add("Mon - Sunny - 88/63");
+        weatherPrevisions.add("Thu - Sunny - 88/63");
+        weatherPrevisions.add("Wed - Sunny - 88/63");
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
