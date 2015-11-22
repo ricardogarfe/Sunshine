61adbc646806195cdfd157d460d27f67ca28d2c0
Ricardo García Fernández <ricardogarfe@gmail.com>
2015-11-22 13:59:03 +0100

    [ci  skip] - Launch SettingsActivity from Main and Detail.

--- app/src/main/java/com/ricardogarfe/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/ricardogarfe/sunshine/app/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.ricardogarfe.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,8 +44,8 @@ public class DetailActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
