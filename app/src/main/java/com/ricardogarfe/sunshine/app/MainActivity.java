a157d6c45386f33a711def6c3b93c06e91081918
Ricardo García Fernández <ricardogarfe@gmail.com>
2015-12-29 18:42:51 +0100

    [ci  skip] - Extend ActionBarActivity on MainActivity

--- app/src/main/java/com/ricardogarfe/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ricardogarfe/sunshine/app/MainActivity.java
@@ -3,12 +3,12 @@ package com.ricardogarfe.sunshine.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback {
+public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
