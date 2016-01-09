278f9b727a3e6cb9d879980ded95a6fb37e85e6c
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-01-09 19:19:30 +0100

    [ci  skip] - Scheduled Synchronization

--- app/src/main/java/com/ricardogarfe/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ricardogarfe/sunshine/app/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ricardogarfe.sunshine.app.sync.SunshineSyncAdapter;
+
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -44,6 +46,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
 
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
 
     @Override
